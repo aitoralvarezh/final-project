0ef7366406d957a1a6d78b73c16509954a80d141
aitoralvarezh <hermida703@gmail.com>
2020-11-08 13:41:56 +0100

    añado proyecto final

---
+++ www/js/main.js
@@ -0,0 +1,13 @@
+console.log('¡Hola mundo!');
+
+const logo = document.querySelector('#logo')
+let displayMenu = e =>{
+    
+
+
+
+}
+console.log(logo);
+
+
+logo.addEventListener('click', displayMenu )
